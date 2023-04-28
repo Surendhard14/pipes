c48ca8acd24cf7669ffa050ef49590eb1c18d9c3
Surendhar D(surendhar.d) <surendhar.d@infosys.com>
2023-04-28 15:35:31 +0530

    Initial Setup Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string | undefined;
+  date: string | undefined;
+  amount: number | undefined;
+  height: number | undefined;
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000,
+  };
+  miles: number | undefined;
+  onNameChange(value: string) {
+    this.name = value;
+  }
+  onDateChange(value: string) {
+    this.date = value;
+  }
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
+  onHeightChange(value: string) {
+    this.height = parseFloat(value);
+  }
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value);
+  }
 }
